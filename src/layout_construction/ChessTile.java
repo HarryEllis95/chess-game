220f468615474fd80a13d4334c549baea2373aa6
Harry Ellis <harrysellis@hotmail.co.uk>
2021-12-08 12:37:49 +0000

    Making the ChessTile class immutable

--- src/layout_construction/ChessTile.java
+++ src/layout_construction/ChessTile.java
@@ -1,22 +1,37 @@
 package layout_construction;
 
+import java.util.*;
+import com.google.common.collect.ImmutableMap;
 
-
+/* Create abstract class to define implementation details of the chess tiles. I want this to be immutable */
 public abstract class ChessTile {
 
-	int tileCoordinate;
-	public ChessTile(int tileCoordinate) {
-		this.tileCoordinate = this.tileCoordinate;
+	private static final Map<Integer, EmptyTile> Empty_Tiles = createAllEmptyTiles();
+	protected final int tileCoordinate;
+	
+	ChessTile(int tileCoordinate) {
+		this.tileCoordinate = tileCoordinate;
 	}
 	
+	private static Map<Integer, EmptyTile> createAllEmptyTiles() {
+		final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+		for(int i=0; i<64; i++) {
+			emptyTileMap.put(i, new EmptyTile(i));  // puts 64 empty tile objects into this map 
+		}
+		// I don't want the map object to be mutable. A common way to achieve this is using Guava's Immutable Map
+		return ImmutableMap.copyOf(emptyTileMap);
+	}
+
 	public abstract boolean isTileOccupied();
 	
 	public abstract ChessPiece getPiece();
 	
-	
+	/* Using a static inner class to define EmptyTile and ChessTile, instead of putting them in seperates files. 
+	 * Due to the nature of them being small simple classes related to the construction of the tiles, I felt this fitted 
+	 * better. Made static to avoid them having inherited state from tile. */
 	public static final class EmptyTile extends ChessTile {
 		
-		EmptyTile(int coordinate) {
+		EmptyTile(final int coordinate) {
 			super(coordinate);
 		}
 		
@@ -32,7 +47,7 @@ public abstract class ChessTile {
 	
 	public static final class OccupiedTile extends ChessTile {
 		
-		ChessPiece pieceOnTile;
+		private final ChessPiece pieceOnTile;
 		
 		OccupiedTile(int tileCoordinate, ChessPiece pieceOnTile) {
 			super(tileCoordinate);
@@ -48,3 +63,6 @@ public abstract class ChessTile {
 		}
 	}
 }
+
+
+
